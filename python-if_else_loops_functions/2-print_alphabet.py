d8393d17f0cf9034852cc83fa0963281cfe096d0
hruiz <elproferuiz@yahoo.com>
2025-01-17 15:25:14 -0400

    alfabeto en minusculas 3

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for i in range(97, 123):  # ASCII values for 'a' to 'z'
-    print("{}".format(chr(i), end=""))  # Convert to character and print
+    print("{}".format(chr(i)), end="")  # Convert to character and print
