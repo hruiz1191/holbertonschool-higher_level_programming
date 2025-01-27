a5fb8906b4c9c78a3d232b64211a232077a2ced6
hruiz <elproferuiz@yahoo.com>
2025-01-26 22:30:00 -0400

    roman to integer 10

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,35 @@
 #!/usr/bin/python3
+
 def roman_to_int(roman_string):
-    sum = 0
-    if(roman_string and isinstance(roman_string, str)):
-        iT = {'D': 500, 'C': 100, 'M': 1000, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
-        aList = [iT.get(x) for x in roman_string]
-        for idx, char in enumerate(aList):
-            if(idx < len(aList) - 1):
-                if aList[idx + 1] > char:
-                    sum -= char
-                else:
-                    sum += char
-            else:
-                sum += char
-    return(sum)
+    if not isinstance(roman_string, str) or not roman_string:
+        return 0
+
+    roman_dict = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+    num = 0
+    s1 = "Invalid character '"
+    s2 = "' in Roman numeral string."
+
+    for i in range(len(roman_string)):
+        if roman_string[i] not in roman_dict:
+            raise ValueError("{}{}{}".format(s1, roman_string[i], s2))
+
+        current_value = roman_dict[roman_string[i]]
+        try:
+            next_value = roman_dict[roman_string[i + 1]]
+        except IndexError:
+            next_value = 0
+
+        if current_value < next_value:
+            num -= current_value
+        else:
+            num += current_value
+
+    return num
