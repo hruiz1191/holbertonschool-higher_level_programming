4a278d992ebb7a5bce3972bf71f78227a0431818
hruiz <elproferuiz@yahoo.com>
2025-02-09 13:09:11 -0400

    abc inheritance task 0 to  5

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+__import__("sys")
+
+
+class VerboseList(list):
+    """List modifying Class"""
+    
+    
+    def append(self, object) :
+        print(f"Added [{object}] to the list.")
+        return super().append(object)
+
+    def extend(self, iterable):
+        print(f"Extended the list with [{len(iterable)}] items.")
+        return super().extend(iterable)
+    
+    def remove(self, value):
+        print(f"Removed [{value}] from the list.")
+        return super().remove(value)
+    
+    def pop(self, index=-1):
+        print(f"Popped [{self[index]}] from the list.")
+        return super().pop(index)
---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+class CountedIterator:
+    """Class to mimic iter"""
+    def __init__(self, data):
+        self.data = iter(data)
+        self.counter = 0
+
+    def get_count(self):
+        """returns count"""
+        return self.counter
+
+    def __next__(self):
+        """override __next__"""
+        try:
+            item = next(self.data)
+            self.counter += 1
+            return item
+        except StopIteration:
+            raise StopIteration
