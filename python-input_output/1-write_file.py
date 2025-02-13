9d7ed1f4cac75ffbaad702445df2df8019d581c3
hruiz <elproferuiz@yahoo.com>
2025-02-13 14:43:27 -0400

    task 1

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!usr/bin/pythom3
+"""This program writes a file using python"""
+
+
+def write_file(filename="", text=""):
+    """Reads a text file (UTF8) and prints its content to stdout."""
+    with open(filename, "w", encoding="utf-8") as file:
+        file.write(text)
