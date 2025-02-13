ab14dbba456c660e7204faf5f93d53f74ca9b913
hruiz <elproferuiz@yahoo.com>
2025-02-13 14:22:54 -0400

    task 0 try 5

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-""" """
+"""Module that reads a text file (UTF8) and prints it to stdout."""
 
 def read_file(filename=""):
-    """Reads a text file (UTF8) and prints it to stdout."""
-
-    with open(filename, encoding="utf-8") as file:
-        print(file.read(), end="")  # Imprime el contenido del archivo
+    """Reads a text file (UTF8) and prints its content to stdout."""
+    with open(filename, "r", encoding="utf-8") as file:
+        print(file.read(), end="")
