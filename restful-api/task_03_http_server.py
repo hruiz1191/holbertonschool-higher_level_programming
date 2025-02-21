7f7143764d651204ca6bc8f24236212959be87ab
hruiz <elproferuiz@yahoo.com>
2025-02-21 14:24:52 -0400

    task 2 try 4

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -18,8 +18,8 @@ class SimpleAPIHandler(BaseHTTPRequestHandler):
             self._send_json_response(200, data)
 
         elif self.path == "/status":
-            status_response = {"status": "OK"}
-            self._send_json_response(200, status_response)
+            status_response = {"status": "ok"}  # El test probablemente espera "ok" en minúscula
+             self._send_json_response(200, status_response)
 
         elif self.path == "/info":
             info_response = {
