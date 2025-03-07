b94557eafd6fc68fe2c54c69dd748f1be4762837
hruiz <elproferuiz@yahoo.com>
2025-03-07 15:52:02 -0400

    task 2 try 3

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,4 @@
--- Crear la base de datos (si no existe)
+--creates the MySQL server user user_0d_2
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
--- Crear el usuario (si no existe) con contraseña
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
