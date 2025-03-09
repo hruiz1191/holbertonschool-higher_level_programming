44794e28d0b4b5ef0766b06bae30c71691bbc342
hruiz <elproferuiz@yahoo.com>
2025-03-09 19:51:25 -0400

    task 0 try 3

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,36 @@
 #!/usr/bin/python3
-"""Lists states"""
-
+"""Lists all states in a database"""
 import MySQLdb
-from sys import argv
+import sys
+
+
+def list_states(username, password, database_name):
+    """Connects to MySQL server"""
+
+    try:
+        conn = MySQLdb.connect(host='localhost',
+                               port=3306,
+                               user=username,
+                               password=password,
+                               db=database_name)
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+        states = cursor.fetchall()
+        for state in states:
+            print(state)
+
+        cursor.close()
+        conn.close()
+
+    except MySQLdb.Error as e:
+        print(f"Error connectig to MySQL or execuiting query: {e}")
+
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                           passwd=argv[2], db=argv[3], charset="utf8")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-    cur.close()
-    conn.close()
+    if len(sys.argv) != 4:
+        print("Usage: python3 0-selsct_states.py username password database")
+    else:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        database_name = sys.argv[3]
+        list_states(username, password, database_name)
