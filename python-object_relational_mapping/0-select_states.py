afbe2e14c24b9095eac3567bd7d4eae12a9c48c7
hruiz <elproferuiz@yahoo.com>
2025-03-09 19:54:41 -0400

    task 0 try 5

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,36 +1,16 @@
 #!/usr/bin/python3
-"""Lists all states in a database"""
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
 
-def list_states(username, password, database_name):
-    """Connects to MySQL server"""
-
-    try:
-        conn = MySQLdb.connect(host='localhost',
-                               port=3306,
-                               user=username,
-                               password=password,
-                               db=database_name)
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
-        states = cursor.fetchall()
-        for state in states:
-            print(state)
-
-        cursor.close()
-        conn.close()
-
-    except MySQLdb.Error as e:
-        print(f"Error connectig to MySQL or execuiting query: {e}")
-
-
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: python3 0-selsct_states.py username password database")
-    else:
-        username = sys.argv[1]
-        password = sys.argv[2]
-        database_name = sys.argv[3]
-        list_states(username, password, database_name)
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
